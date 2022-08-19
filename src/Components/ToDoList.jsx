2f0f6102dfa3fd89a28092eb1ce5b1f51a62a2df
Andrew Wand <andrewwands@gmail.com>
2022-08-18 22:49:14 -0500

    started edit button logic

--- src/Components/ToDoList.jsx
+++ src/Components/ToDoList.jsx
@@ -1,12 +1,32 @@
 import React from 'react'
+import { useState } from 'react';
 
 function ToDoList(props) {
   
   
-const showTaskList = (
+  
+  const [isEdit, setIsEdit] = useState(false);
+
+
+  const handleSubmit = () => {
+      
+  }
+
+  const showEdit = (
+      <form action="submit" className="edit-form" onSubmit={handleSubmit}>
+          
+      </form>
+  )
+
+
+
+  const showTaskList = (
     <div className="task-container">
       <div>
-        <input id={props.id} type="checkbox" defaultChecked={props.completed} onChange={() => props.toggleTaskCompleted(props.id)} />
+        <input id={props.id} 
+              type="checkbox" 
+              defaultChecked={props.completed} 
+              onChange={() => props.toggleTaskCompleted(props.id)} />
         <label htmlFor={props.id} className="todo-label">{props.name}</label>
         <button className="delete-btn" onClick={() => props.deleteTask(props.id)}>Delete</button>
       </div>
